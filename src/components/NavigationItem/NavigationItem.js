496fe5a96c71fe53bc1ed20b78fd0fa5ed9c9007
IiIymik <sambotemp@gmail.com>
2021-08-18 21:37:55 +0300

    add Navigation Item

---
+++ src/components/NavigationItem/NavigationItem.js
@@ -0,0 +1,6 @@
+import styles from './NavigationItem.module.css'
+export default function NavigationItem({ text, icon }) {
+    return <li className={styles.item}>
+        {icon} {text}
+    </li>
+};
